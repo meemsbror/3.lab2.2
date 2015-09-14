9a354c75284cb0e13e4447639d68fd8dbd023c23
Frej Karlsson <frejlekarlsson@gmail.com>
2015-09-15 00:05:40 +0200

    Vi testar bara
    faktiskt bara test

---
+++ src/RatNum.java
@@ -0,0 +1,30 @@
+/**
+ * Created by Boy on 2015-09-14.
+ */
+public class RatNum {
+
+    public static int gcd(int x, int y){
+        int m = x;
+        int n = y;
+        int r;
+
+        if((m == 0 && n == 0))
+            throw new IllegalArgumentException("both values were zero");
+        if(n == 0)
+            return (m);
+
+        if(m<0){
+            m = m-(m*2);
+        }
+        if(n<0){
+            n = n-(n*2);
+        }
+
+        while (m%n>=1){
+            r= m%n;
+            m=n;
+            n=r;
+        }
+        return n;
+    }
+}
